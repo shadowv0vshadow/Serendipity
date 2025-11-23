05bcce9c3a97cef945147fa0d0c798c9da73130a
shadowv0vshadow <154503348+shadowv0vshadow@users.noreply.github.com>
2025-11-23 16:12:46 +0900

    Move rewrites to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/index',
+      },
+    ];
+  },
   reactCompiler: true,
 };
 
